e4d7a5886d83e2c1d3f41d35b50bce50d58eb091
mozart25 <mozartlee28@gmail.com>
2019-06-03 17:14:15 +0900

    first commit

---
+++ src/Components/Resultbar.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './Resultbar.scss';
+import { withRouter } from 'react-router-dom';
+
+class Resultbar extends React.Component {
+
+
+  render() {
+
+    return (
+      <div>
+        <div className="Rectangle"> </div>
+      <div>
+
+    );
+  }
+}
+
+export default withRouter(Resultbar);
---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Report from './Pages/Reportai';
+
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/report" component={Report} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+
+export default Routes;
