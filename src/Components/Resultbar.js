62c328f3cffa536f6563c68178ad68000cea46ed
mozart25 <mozartlee28@gmail.com>
2019-06-03 22:29:36 +0900

    브랜치 교체를 위한 마스터 브랜치 생성 및 환경세팅

---
+++ src/Components/Resultbar.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './Resultbar.scss';
+import { withRouter } from 'react-router-dom';
+
+class Resultbar extends React.Component {
+
+
+  render() {
+
+    return (
+
+    );
+  }
+}
+
+export default withRouter(Resultbar);
